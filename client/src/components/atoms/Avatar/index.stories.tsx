91e2e4521f47e1c3cd0c8e13c9feba9f421131e9
dnrlwo <dnrlwo11@gmail.com>
2021-02-08 17:34:06 +0900

    test: avatar componnet

--- client/src/components/atoms/Avatar/index.stories.tsx
+++ client/src/components/atoms/Avatar/index.stories.tsx
@@ -1,22 +1,10 @@
 import React from 'react';
-import { text, boolean, select } from '@storybook/addon-knobs';
-import { action } from '@storybook/addon-actions';
+import { text } from '@storybook/addon-knobs';
 
 import Avatar from '.';
 
 export default {
   title: 'Atoms / Avatar',
-};
-
-const styleTypeS = {
-  label: 'buttonType',
-  options: [
-    'default',
-    'primary',
-    'link',
-    'text'
-  ],
-  defaultValue: 'default',
 };
 
 export const general: React.FC = () => (
@@ -26,36 +14,98 @@ export const general: React.FC = () => (
 );
 
 export const allstyle: React.FC = () => (
-  <div
-    style={{
-      display: 'flex',
-      justifyContent: 'space-between',
-      alignItems: 'flex-start',
-      alignContent: 'space-between',
-      width: '100%'
-    }}
-  >
-    <Avatar
-      children={'User'}
-      shape="circle"
-      src="https://avatars.githubusercontent.com/u/56660101?s=460&u=2b393f9517326acb86953360425d06162340d849&v=4"
-    />
-    <Avatar
-      size={'large'}
-      children={'User'}
-      shape="square"
-    />
-    <Avatar
-      size={'large'}
-      src="https://avatars.githubusercontent.com/u/56660101?s=460&u=2b393f9517326acb86953360425d06162340d849&v=4"
-    />
-    <Avatar
-      size={'small'}
-      children={'User'}
-    />
-    <Avatar
-      size={'small'}
-      children={'Jaewook'}
-    />
-  </div>
+  <>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'flex-start',
+        alignContent: 'space-between',
+        width: '100%'
+      }}
+    >
+      <Avatar
+        children={'User'}
+        shape="circle"
+        src="https://avatars.githubusercontent.com/u/56660101?s=460&u=2b393f9517326acb86953360425d06162340d849&v=4"
+      />
+      <Avatar
+        size={'large'}
+        children={'User'}
+        shape="square"
+      />
+      <Avatar
+        size={'large'}
+        src="https://avatars.githubusercontent.com/u/56660101?s=460&u=2b393f9517326acb86953360425d06162340d849&v=4"
+      />
+      <Avatar
+        size={'small'}
+        children={'User'}
+      />
+      <Avatar
+        size={'large'}
+        children={'User'}
+      />
+      <Avatar
+        size={'small'}
+        children={'Jaewook'}
+      />
+      <Avatar
+        size={60}
+        gap={2}
+      >
+        정재욱
+      </Avatar>
+      <Avatar
+        size={'small'}
+        children={'gap1'}
+        gap={1}
+      />
+      <Avatar
+        size={'small'}
+        children={'gap2'}
+        gap={2}
+      />
+      <Avatar
+        size={'small'}
+        children={'gap3'}
+        gap={3}
+      />
+    </div>
+    <div style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'flex-start',
+        alignContent: 'space-between',
+        width: '100%'
+      }}>
+        <Avatar
+        size={'large'}
+        children={'gap1'}
+        gap={1}
+        />
+        <Avatar
+          size={'large'}
+          children={'gap2'}
+          gap={2}
+        />
+        <Avatar
+          size={'large'}
+          children={'gap3'}
+          gap={3}
+        />
+        <Avatar
+          size={'large'}
+          children={'gap4'}
+        />
+        <Avatar
+          style={{color: 'rgb(245, 106, 0)', backgroundColor: 'rgb(253, 227, 207)'}}
+          children="M"
+        />
+        <Avatar
+          size={50}
+          src="https://avatars.githubusercontent.com/u/56660101?s=460&u=2b393f9517326acb86953360425d06162340d849&v=4"
+      />
+    </div>
+  </>
 );
