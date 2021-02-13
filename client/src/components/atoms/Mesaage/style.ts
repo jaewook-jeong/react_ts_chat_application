8875753e687c7cb55cb738d88752a819db5f4111
dnrlwo <dnrlwo11@gmail.com>
2021-02-13 21:06:42 +0900

    chore: edit message style

--- client/src/components/atoms/Mesaage/style.ts
+++ client/src/components/atoms/Mesaage/style.ts
@@ -8,14 +8,16 @@ interface Props {
 export const MessageContainer = styled.div<Props>`
 width: 100%;
 display: flex;
+font-size: .9375rem;
+padding-bottom: 2px;
 ${props => {
   if (props.type === 'me') {
     return css`
-      justify-content: flex-start;
+      justify-content: flex-end;
     `;
   } else {
     return css`
-      justify-content: flex-end;
+      justify-content: flex-start;
     `;
   }
 }}
@@ -23,16 +25,30 @@ ${props => {
 
 export const MeContainer = styled.div`
 padding: 0px 10px;
+div:nth-child(2) {
+  background-color: #2f74c0;
+  color: #fff;
+}
 `;
 
 export const OtherContainer = styled.div`
 padding: 0px 10px;
+div:nth-child(1) {
+  background-color: #e4e6eb;
+  color: #050505;
+}
 `;
 
 export const TimeWrapper = styled.div`
-color: #ddd;
+color: #ccc;
+display: inline-block;
+font-size: 12px;
+margin: 0px 5px;
+vertical-align: bottom;
 `;
 
 export const ContentWrapper = styled.div`
-
+display: inline-block;
+padding: 8px 12px;
+border-radius: 18px;
 `;
