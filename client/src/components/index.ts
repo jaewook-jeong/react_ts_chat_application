df1aa0315e4490a8db69400e45736359dfad005c
dnrlwo <dnrlwo11@gmail.com>
2021-02-14 23:19:33 +0900

    chore: index comp

---
+++ client/src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Avatar } from './atoms/Avatar';
+export { default as Button } from './atoms/Button';
+export { default as Loading } from './atoms/Loading';
+export { default as Message } from './atoms/Message';
