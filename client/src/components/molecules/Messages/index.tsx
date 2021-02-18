11338f233791f5e813ebd98d0c96fd5ed80fa4a7
dnrlwo <dnrlwo11@gmail.com>
2021-02-18 20:55:06 +0900

    feat: messages component

---
+++ client/src/components/molecules/Messages/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { MessagesContainer } from './style';
+
+interface Props {
+
+}
+
+function Messages({}: Props) {
+  return (
+    <MessagesContainer>
+      
+    </MessagesContainer>
+  );
+}
+
+export default Messages;
