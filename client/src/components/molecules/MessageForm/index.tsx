759bef2aae65919351ae5dadb40945ff738594de
dnrlwo <dnrlwo11@gmail.com>
2021-02-20 23:52:25 +0900

    feat: add Message Form molcule

---
+++ client/src/components/molecules/MessageForm/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { MessageFormWrapper } from './style';
+
+interface Props {
+
+}
+
+function MessageForm({}: Props) {
+  return (
+    <MessageFormWrapper>
+      
+    </MessageFormWrapper>
+  );
+}
+
+export default MessageForm;
